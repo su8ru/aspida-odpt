184d55edb1025eb1284b00caa54b75c58a335bab
subaru <s@su8ru.dev>
2020-07-02 18:50:31 +0900

    fix: ? position

--- v4/odpt%3ACalendar/@types.ts
+++ v4/odpt%3ACalendar/@types.ts
@@ -2,13 +2,13 @@ import { OdptBase } from "../@types";
 
 export interface Calender extends OdptBase {
   // カレンダー名称（日本語）。e.g. 平日、休日、市場休日
-  "dc:title?": string;
+  "dc:title"?: string;
   // ァレンダー名称（多言語対応）
-  "odpt:calenderTitle?": CalenderTitle;
+  "odpt:calenderTitle"?: CalenderTitle;
   // 該当する日付のリスト。値は全てISO8601の日付形式。
-  "odpt:day?": string[];
+  "odpt:day"?: string[];
   // カレンダー有効期間。値は全てISO8601の期間形式。
-  "odpt:duration?": string;
+  "odpt:duration"?: string;
 }
 
 export interface CalenderTitle {
--- v4/odpt%3AOperator/@types.ts
+++ v4/odpt%3AOperator/@types.ts
@@ -2,9 +2,9 @@ import { OdptBase } from "../@types";
 
 export interface Operator extends OdptBase {
   // 事業者名称（日本語）
-  "dc:title?": string;
+  "dc:title"?: string;
   // 事業者名称（多言語対応）
-  "odpt:operatorTitle?": OperatorTitle;
+  "odpt:operatorTitle"?: OperatorTitle;
 }
 
 export interface OperatorTitle {
