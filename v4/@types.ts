8461e578eb7e89e65a3f16bd3b630c4c246cb4e8
subaru <s@su8ru.dev>
2020-07-02 18:45:12 +0900

    feat: add comments

--- v4/@types.ts
+++ v4/@types.ts
@@ -1,7 +1,15 @@
 export interface OdptBase {
+  // JSON-LD仕様に基づく @context のURL
   "@context": string;
+  // 固有識別子(ucode)
   "@id": string;
+  // クラス指定
   "@type": string;
+  // データ生成日時（ISO8601 日付時刻形式）
   "dc:date"?: string;
+  // 固有識別子。
   "owl:SameAs": string;
 }
+
+let hoge: OdptBase;
+let as = hoge["@context"]
--- v4/odpt%3AOperator/@types.ts
+++ v4/odpt%3AOperator/@types.ts
@@ -1,7 +1,9 @@
 import { OdptBase } from "../@types";
 
 export interface Operator extends OdptBase {
+  // 事業者名称（日本語）
   "dc:title?": string;
+  // 事業者名称（多言語対応）
   "odpt:operatorTitle?": OperatorTitle;
 }
 
