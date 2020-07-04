becfdf3effb946acf2a3925b23d9d3e0408a3df0
subaru <s@su8ru.dev>
2020-07-04 20:12:41 +0900

    feat: add odot:Railway

---
+++ v4/odpt%3ARailway/@types.ts
@@ -0,0 +1,42 @@
+import { OdptBase } from "../@types";
+
+export interface Railway extends OdptBase {
+  // 路線名（日本語）
+  "dc:title": string;
+  // 路線名（多言語対応）
+  "odpt:operatorTitle"?: RailwayTitle;
+  // 路線名のよみがな（ひらがな表記）
+  "odpt:kana"?: string;
+  // 運行会社を表すID (odpt:Operatorのowl:sameAs)
+  "odpt:operator": string;
+  // 路線コード、路線シンボル表記を格納。e.g. 丸ノ内線=>M
+  "odpt:lineCode"?: string;
+  // 路線のラインカラーをHEX表記#RRGGBB、DIC表記DICnnn、PANTONE表記PANTONExxxxで表記する
+  "odpt:color"?: string;
+  // GeoJSON形式による地物情報
+  "ug:region"?: object;
+  // 昇順の進行方向を表すID。odpt:stationOrderのodpt:indexの昇順方向を、odpt:RailDirectionのowl:sameAsで表したIDで格納する。
+  "odpt:ascendingRailDirection"?: string;
+  // 降順の進行方向を表すID。odpt:stationOrderのodpt:indexの降順方向を、odpt:RailDirectionのowl:sameAsで表したIDで格納する。
+  "odpt:descendingRailDirection"?: string;
+  // 駅の順序を表すリスト
+  "odpt:stationOrder": StationOrder[];
+}
+
+export interface StationOrder {
+  // 駅を表すID (odpt:Stationのowl:sameAs)
+  "odpt:station": string;
+  // 駅名（多言語対応）
+  "odpt:stationTitle"?: StationTitle;
+  // 駅の順序を示す整数値。原則として、列車は進行方向に応じて、この値の昇順または降順に停車する。環状線などの場合は、同一の駅が複数回記載される場合がある。
+  "odpt:index": number;
+}
+
+export interface RailwayTitle {
+  ja: string;
+  en: string;
+}
+export interface StationTitle {
+  ja: string;
+  en: string;
+}
---
+++ v4/odpt%3ARailway/index.ts
@@ -0,0 +1,18 @@
+import { Railway } from "./@types";
+
+export interface Methods {
+  get: {
+    query: {
+      "acl:consumerKey": string;
+      "@id"?: string;
+      "owl:sameAs"?: string;
+      // 路線名（e.g. 小田原線、京王線、相模原線）
+      "dc:title"?: string;
+      // 事業者を表すID (odpt:Operatorのowl:sameAs)
+      "odpt:operator"?: string;
+      // 路線コード、路線シンボル表記（e.g. 小田原線 => OH、丸ノ内線 => M）
+      "odpt:lineCode"?: string;
+    };
+    resBody: Railway;
+  };
+}
