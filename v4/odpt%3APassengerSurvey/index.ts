834824a47e2f2b10cfc253fb297a1cbe0443b37e
subaru <s@su8ru.dev>
2020-07-08 01:13:32 +0900

    fix: add query comments

--- v4/odpt%3APassengerSurvey/index.ts
+++ v4/odpt%3APassengerSurvey/index.ts
@@ -6,8 +6,11 @@ export interface Methods {
       "acl:consumerKey": string;
       "@id"?: string;
       "owl:sameAs"?: string;
+      // 事業者を表すID (odpt:Operatorのowl:sameAs)
       "odpt:operator"?: string;
+      // 駅を表すID (odpt:Stationのowl:sameAs)
       "odpt:station"?: string;
+      // 路線を表すID (odpt:Railwayのowl:sameAs)
       "odpt:railway"?: string;
     };
     resBody: PassengerSurvey;
