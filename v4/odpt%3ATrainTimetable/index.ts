ed12ef01cc334d0d16229918a542b97a5126616b
subaru <s@su8ru.dev>
2020-07-08 01:10:05 +0900

    feat: add odpt:TrainTimetable

---
+++ v4/odpt%3ATrainTimetable/index.ts
@@ -0,0 +1,24 @@
+import { TrainTimetable } from "./@types";
+
+export interface Methods {
+  get: {
+    query: {
+      "acl:consumerKey": string;
+      "@id"?: string;
+      "owl:sameAs"?: string;
+      // 列車番号
+      "odpt:trainNumber"?: string;
+      // 路線のID (odpt:Railwayのowl:sameAs)
+      "odpt:railway"?: string;
+      // 運行事業者のID (odpt:Operatorのowl:sameAs)
+      "odpt:operator"?: string;
+      // 列車種別ID (odpt:TrainTypeのowl:sameAs)
+      "odpt:trainType"?: string;
+      // 該当する列車ID (odpt:Trainのowl:sameAs)
+      "odpt:train"?: string
+      // 特定のカレンダー情報ID (odpt:Calendarのowl:sameAs)
+      "odpt:calendar"?: string;
+    };
+    resBody: TrainTimetable;
+  };
+}
